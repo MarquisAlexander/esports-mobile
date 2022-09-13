a1bf8f814ccdcf1f81e471b4a2f3405fb4cf0172
MarquisAlexander <marquis.alexander@outlook.com>
2022-09-13 20:54:35 -0300

    feat():first commit

---
+++ src/components/Heading/index.tsx
@@ -0,0 +1,21 @@
+import { View, Text, ViewProps } from 'react-native';
+
+import { styles } from './styles';
+
+interface Props extends ViewProps {
+    title: string;
+    subTitle: string;
+}
+
+export function Heading({title, subTitle, ...props}: Props) {
+  return (
+    <View style={styles.container} {...props}>
+<Text style={styles.title}>
+    {title}
+</Text>
+<Text style={styles.subtitle}>
+    {subTitle}
+</Text>
+    </View>
+  );
+}
